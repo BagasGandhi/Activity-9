7213cd807e769e1a9b4f623f0dbbcd71f8a3515c
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-05-05 15:48:12 +0700

    Define Mahasiswa class with static member variable and instance variables

--- staticmember.cpp
+++ staticmember.cpp
@@ -3,4 +3,11 @@ using namespace std;
 
 class Mahasiswa { //membuat class Mahasiswa
 
+public:
+    static int nim; //deklarasi variable static yaitu variable nim
+    //deklarasi variable member
+    int id;
+    string nama;
+
+    
 };
