66e3d7e0c1c6842f8f0f9a9a918d9475b0e6deca
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-05-05 16:02:32 +0700

    Refactor Mahasiswa class to include private static member and public methods

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -4,5 +4,17 @@ using namespace std;
 
 class Mahasiswa {
 
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
 
+    void setID();
+    void printAll();
+
+    //pembuatan static function
+    static void setNim(int pnim) {
+        nim = pnim;
+    };
 };
