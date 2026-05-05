326a4a777b3273729d342f4243e88e22ed5c3735
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-05-05 17:10:34 +0700

    Add main function to demonstrate usage of setget method in buku class

--- this.cpp
+++ this.cpp
@@ -10,5 +10,12 @@ class buku{
             this->judul = judul; // nilai parameter 'judul' untuk member variabel 'judul'
             return this->judul;  // return variable judul.
         }
-        
+
 };
+
+int main()
+{
+    buku bukunya;
+    cout<<bukunya.setget ("Matematika");
+    return 0;
+}
