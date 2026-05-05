c84eef4f866738c73c86d5a4c573ff95207cdd96
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-05-05 17:15:55 +0700

    Add main function to demonstrate chain function calls in buku class

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -12,5 +12,14 @@ class buku{
     string getJudul(){
         return this->judul;
     }
-    
+
 };
+
+int main()
+{
+    buku bukunya;
+    //bukunya.setJudul("Matematika")'
+    //cout << bukunya.getJudul()<< endl;
+    cout<<bukunya.setJudul("Matematika").getJudul(); //chain function calls
+    return 0;
+}
