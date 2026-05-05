260bb0c43d3067b2efd6dd1f711237fe8994ce5e
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-05-05 16:21:32 +0700

    Define constructor implementation for mahasiswa class in constructorberparameter.cpp

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -6,3 +6,10 @@ class mahasiswa {
 public:
     mahasiswa(int nim, string nama); // Constructor dengan parameter
 };
+
+// Definisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Counstructor dengan parameter terpanggil" << endl;
+    cout << "NIM: " << nim << endl;
+    cout << "Nama: " << nama << endl;
+}
