6fe0946f3729dcd7e47ec02baf49e530ce850e51
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-05-05 16:10:38 +0700

    Fix formatting and add printAll method calls in main function of staticfunction.cpp

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -48,4 +48,16 @@ int main() {
     Mahasiswa::setNim(9);
     Mahasiswa mhs3("Andi Janu");
     Mahasiswa mhs4("Joko Wahono");
+
+    //memanggil prosedur printAll()
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    //menampilkan NIM terakhir yang diproses
+    cout << "Akses dari luar object = " << Mahasiswa::getNim() << endl; 
+    system("pause");
+
+    return 0;
 }
